bab0fc80bca2c159dadedd4da640faf1417d2e93
KyeongHwanMin <h45652@naver.com>
2021-11-19 17:51:58 +0900

    feat : Renderer를 통한 template 응답

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
-
 from rest_framework import generics
 from rest_framework.decorators import api_view, action
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -21,36 +22,36 @@ from .models import Post
 #         return Response(serializer.data)
 
 # 함수 뷰 사용
-# @api_view(['GET'])
-# def public_post_list(request):
-#     qs = Post.objects.filter(is_public=True)
-#     serializer = PostSerializer(qs, many=True)
-#     return Response(serializer.data)
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
 
 
-class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostViewSet(ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#     @action(detail=False, method=['GET'])
+#     def public(self, request):
+#         qs = self.get_queryset().filter(is_public=True)
+#         serializer = self.get_serializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     @action(detail=False, method=['PATCH'])
+#     def set_public(self, request, pk):
+#         instance = self.get_object()
+#         instance.is_public = True
+#         instance.save(update_fields=['is_public'])
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data)
 
-    @action(detail=False, method=['GET'])
-    def public(self, request):
-        qs = self.get_queryset().filter(is_public=True)
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
 
-    @action(detail=False, method=['PATCH'])
-    def set_public(self, request, pk):
-        instance = self.get_object()
-        instance.is_public = True
-        instance.save(update_fields=['is_public'])
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
-
-    # def dispatch(self, request, *args, **kwargs):
-    #     print("request.body :", request.body) # logger 추천
-    #     print("request.POST :", request.POST)
-    #     return super().dispatch(request, *args, **kwargs)
+# def dispatch(self, request, *args, **kwargs):
+#     print("request.body :", request.body) # logger 추천
+#     print("request.POST :", request.POST)
+#     return super().dispatch(request, *args, **kwargs)
 
 # def post_list(request):
 # 2개분기
@@ -58,3 +59,16 @@ class PostViewSet(ModelViewSet):
 # def post_detail(rqeust,pk):
 # request.method => 3개분기
 # pass
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'instagram/post_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+
+        return Response({
+            'post': PostSerializer(post).data,
+        })
