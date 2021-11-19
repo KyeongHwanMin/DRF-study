e60e384e9bbb6a86c87d2702bdbaaad1d3b425f6
KyeongHwanMin <h45652@naver.com>
2021-11-19 16:49:52 +0900

    feat : ModelviewSet 클래스

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-urlpatterns=[
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개 URL을 만든다.
+# router.urls # url pattern list
 
+urlpatterns = [
+    path('public/', views.PublicPostListAPIView.as_view()),
+    path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+# 제네릭뷰사용
+# class PublicPostListAPIView(generics.ListAPIView):
+#     queryset = Post.objects.filter(is_public=True)
+#     serializer_class = PostSerializer
+
+# 클래스 뷰 사용
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+# 함수 뷰 사용
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body :", request.body) # logger 추천
+    #     print("request.POST :", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
+
+# def post_list(request):
+# 2개분기
+# pass
+# def post_detail(rqeust,pk):
+# request.method => 3개분기
+# pass
